b06fb0fb713e4adde84b3228744dfd959c01495b
Brandon Lobo <brandon.lobo11@gmail.com>
2016-07-17 22:28:42 +0530

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,42 @@
 ## functions do
 
 ## Write a short comment describing this function
+## As in the example the function has 4 sections
+## Set: To set the values of the matrix and it's inverse outside of the local
+## environment of the function
+## Get: it fetches the value of the matrix to be used for calculation
+## Setinv: sets the value of the calculated inverse matrix outside the local
+## environment of the function
+## Getinv: It is used to fetch the cached inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat<-NULL
+  set<-function(y){
+    x<<-y
+    invmat<<-NULL
+  }
+  get<-function()x
+  setinv<-function(inverse)invmat<<-inverse
+  getinv<-function()invmat
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
+## The Cachesolve function here accepts the matrix who's inverse needs to be found
+## We then proceed to fetch the inverse of the matrix stored in the global 
+## environment, if it is not null then we return the matrix as the cached inverse
+## If the value of the cached inverse is null then we calculate the inverse of 
+## of the matrix and we cache it for future use and return the inverse for use.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invmat<-x$getinv()
+  if(!is.null(invmat)){
+    message("Getting Cached Data")
+    return(invmat)
+  }
+  mat.data<-x$get()
+  invmat<-solve(mat.data,...)
+  x$setinv(invmat)
+  return(invmat)
 }
